8aa7df635baaa75b10fc6d0a7d63f232dabc677c
Pavle Stepanić <pavlestepanic93@gmail.com>
2022-05-24 20:49:02 +0200

    Initial commit(Spring initializr)

---
+++ src/main/java/com/releasetracker/Releasetracker/ReleaseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.releasetracker.Releasetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseTrackerApplication.class, args);
+	}
+
+}
